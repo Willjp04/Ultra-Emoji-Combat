e03fa3b71d0c75364718d65a5574ce1343ba66cc
Willian Sostenes <sostenes655@gmail.com>
2020-10-07 19:59:14 -0300

    Criação dos métodos ações para luta

--- UltraEmojiCombat/src/ultraemojicombat/Luta.java
+++ UltraEmojiCombat/src/ultraemojicombat/Luta.java
@@ -10,5 +10,66 @@ package ultraemojicombat;
  * @author PORTOMASTER
  */
 public class Luta {
-    
+       private Lutador desafiado;
+       private Lutador desafiante;
+       private int rounds;
+       private boolean aprovado;
+       
+       
+       public void marcarLuta(Lutador l1,Lutador l2){
+           if (l1.getCategoria().equals(l2.getCategoria())
+               && l1 != l2){
+               this.aprovado = true;
+               this.desafiado = l1;
+               this.desafiante = l2;
+               
+           } else {
+               this.aprovado = true;
+               
+               
+           
+           
+               
+           }
+       }
+       
+       public void lutar(){
+           
+           
+           
+       }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+       
+               
 }
